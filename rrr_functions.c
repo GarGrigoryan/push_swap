b96b68f26b793d406655e2df0decc3b33e674c11
Garik <garikyang950@gmail.com>
2026-03-05 22:43:29 +0400

    functions added to header

--- rrr_functions.c
+++ rrr_functions.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   rrr_functions.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gargrigo <gargrigo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/05 18:34:31 by gargrigo          #+#    #+#             */
-/*   Updated: 2026/03/05 22:42:59 by gargrigo         ###   ########.fr       */
+/*   Created: 2026/03/05 22:43:06 by gargrigo          #+#    #+#             */
+/*   Updated: 2026/03/05 22:43:07 by gargrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-typedef struct s_node
-{
-	struct s_node	*next;
-	int				num;
-}	t_node;
-
-typedef struct s_list
-{
-	t_node	*top;
-}	t_stack;
-
-void	sa(t_stack *stack);
-void	sb(t_stack *stack);
-void	ss(t_stack *a, t_stack *b);
-void	pa(t_stack *a, t_stack *b);
-void	pb(t_stack *a, t_stack *b);
-void	ra(t_stack *a);
-void	rb(t_stack *b);
-void	rr(t_stack *a, t_stack *b);
-
-#endif
