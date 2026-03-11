170076ecd78be7d962987981263e93d2235d05cf
Arsen Zakaryan <arsezaka@c2r17s1.42yerevan.am>
2026-03-11 19:45:16 +0400

    rest of parser

--- print_stack.c
+++ print_stack.c
@@ -27,28 +27,28 @@ void	print_stack(t_stack *stack)
 	}
 }
 
-int	main(void)
-{
-	t_stack	*stack;
-	t_node *node1;
-	t_node *node2;
-	t_node *node3;
+// int	main(void)
+// {
+// 	t_stack	*stack;
+// 	t_node *node1;
+// 	t_node *node2;
+// 	t_node *node3;
 
-	node1 = malloc(sizeof(t_node));
-	// node2 = malloc(sizeof(t_node));
-	// node3 = malloc(sizeof(t_node));
-	stack = malloc(sizeof(t_stack));
+// 	node1 = malloc(sizeof(t_node));
+// 	// node2 = malloc(sizeof(t_node));
+// 	// node3 = malloc(sizeof(t_node));
+// 	stack = malloc(sizeof(t_stack));
 
-	node1->num = 10;
-	// node2->num = 12;
-	// node3->num = 24;
+// 	node1->num = 10;
+// 	// node2->num = 12;
+// 	// node3->num = 24;
 
-	// node1->next = node2;
-	// node2->next = node3;
-	stack->top = node1;
-	print_stack(stack);
-	sa(stack);
-	printf("\n");
-	print_stack(stack);
-	return (0);
-}
+// 	// node1->next = node2;
+// 	// node2->next = node3;
+// 	stack->top = node1;
+// 	print_stack(stack);
+// 	sa(stack);
+// 	printf("\n");
+// 	print_stack(stack);
+// 	return (0);
+// }
--- push_swap.h
+++ push_swap.h
@@ -1,17 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gargrigo <gargrigo@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/05 18:34:31 by gargrigo          #+#    #+#             */
-/*   Updated: 2026/03/05 23:00:08 by gargrigo         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
 
 typedef struct s_node
 {
@@ -19,21 +12,33 @@ typedef struct s_node
 	int				num;
 }	t_node;
 
-typedef struct s_list
+typedef struct s_stack
 {
 	t_node	*top;
 }	t_stack;
 
+void	parse_arguments(int argc, char **argv, t_stack *stack_a);
+int		ft_atoi(const char *str, int *error);
+int		is_duplicate(t_node *stack, int num);
+int		flag_checker(char **argv);
+char	**free_all_mem(char **mem_seg);
+void	free_stack(t_stack *stack);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+
+t_node	*stack_new(int num);
+void	stack_add_back(t_stack *stack, t_node *new_node);
+
 void	sa(t_stack *stack);
 void	sb(t_stack *stack);
-void	ss(t_stack *a, t_stack *b);
-void	pa(t_stack *a, t_stack *b);
 void	pb(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
 void	ra(t_stack *a);
 void	rb(t_stack *b);
-void	rr(t_stack *a, t_stack *b);
 void	rra(t_stack *a);
 void	rrb(t_stack *b);
-void	rrr(t_stack *a, t_stack *b);
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	**ft_split(char const *s, char c);
 
 #endif
