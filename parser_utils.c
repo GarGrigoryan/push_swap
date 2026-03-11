7612f77fc9df005aaead7ef9f49f7f6074053a8a
Arsen Zakaryan <arsezaka@c2r17s1.42yerevan.am>
2026-03-11 19:41:02 +0400

    parser done

---
+++ parser_utils.c
@@ -0,0 +1,40 @@
+#include "push_swap.h"
+
+t_node	*stack_new(int num)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->num = num;
+	node->next = NULL;
+	return (node);
+}
+
+void	stack_add_back(t_stack *stack, t_node *new_node)
+{
+	t_node	*last;
+
+	if (!stack || !new_node)
+		return ;
+	if (!stack->top)
+	{
+		stack->top = new_node;
+		return ;
+	}
+	last = stack->top;
+	while (last->next)
+		last = last->next;
+	last->next = new_node;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
